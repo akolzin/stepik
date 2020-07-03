f8f4f97323ec082ca970e33f48ef2ec0676137ed
akolzin <59665736+akolzin@users.noreply.github.com>
2020-07-03 14:00:19 +0400

    +

---
+++ corect time.py
@@ -0,0 +1,9 @@
+import pytest
+from selenium import webdriver
+import time
+import math
+
+answer = math.log(int(time.time() - 658))
+print(answer)
+print(time.time())
+print(int(time.time() - 658))
