ceb1c7683188b7dbc35780558752ae1d91a0ab7d
akolzin <59665736+akolzin@users.noreply.github.com>
2020-07-13 15:54:13 +0400

    +

--- tests/test_example.py
+++ tests/test_example.py
@@ -46,20 +46,20 @@ class TestExample:
 
     def test_new_booking_exists(self, client):
         data = random.random_booking()
-        res = client.vr(client.create_booking(data), [200, 201])
+        res = client.create_booking(data)
         created = res.json()
         bookingid = created.get("bookingid")
-        res = client.vr(client.get_booking(bookingid))
+        res = client.get_booking(bookingid)
         exists = res.json()
         assert exists == data
 
     def test_update_booking(self, client):
         data = random.random_booking()
-        res = client.vr(client.create_booking(data), [200, 201])
+        res = client.create_booking(data)
         created = res.json()
         bookingid = created.get("bookingid")
         data2 = random.random_booking()
-        res = client.vr(client.update_booking(bookingid, data2))
+        res = client.update_booking(bookingid, data2)
         updated = res.json()
         assert updated == data2
 
