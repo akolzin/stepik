c2a8d9a286a94cb7af864b98cd0027f0e31d99c1
akolzin <59665736+akolzin@users.noreply.github.com>
2020-09-17 15:27:07 +0400

    +

--- base_page.py
+++ base_page.py
@@ -10,7 +10,7 @@ from selenium.webdriver.support.wait import WebDriverWait
 link = "http://media.test.itass.local/customers"
 
 class BasePage():
-    def __init__(self, browser, url, timeout=10):
+    def __init__(self, browser, url, timeout=5):
         self.browser = browser
         self.url = url
         self.browser.implicitly_wait(timeout)
--- test_tass.py
+++ test_tass.py
@@ -23,12 +23,12 @@ link = "http://media.test.itass.local/customers"
 # class TestMainPage1():
 # вызываем фикстуру в тесте, передав ее как параметр
 # создание внутреннего клиента
-@pytest.mark.parametrize('selector, value, selector1, value1, selector2, value2, selector3, value3',
-                         [("#name", "test1@yandex.ru", "#email", "teывап2@yanывапdex.ru", "#phone1", "test3@yandex.ru", "#site", "test4@yandex.ru"),
-                          ("#email", "teывап2@yanывапdex.ru", 79992244333, "teывап2@yanывапdex.r", 12345, "city", 2020202032, 222444882),
-                          ("#phone1", "test3@yandex.ru", 79992244333, "test3@yandex.r", 12345, "city", 2020202023, 222444883),
-                          ("#site", "test4@yandex.ru", 79992244333, "test4@yandex.r", 12345, "city", 2020202024, 222444884)])
-def test_guest(browser, selector, value, selector1, value1, selector2, value2, selector3, value3):
+@pytest.mark.parametrize('flag, links, buttons, selector, value, selector1, value1, selector2, value2, selector3, value3, button_save',
+                         [("client", "http://media.test.itass.local/clients/customers", "#root > div > div > div > header > a > button", "#name", "test1@yandex.ru", "#email", "teывап2@yanывапdex.ru", "#phone1", "test3@yandex.ru", "#site", "test4@yandex.ru", "#root > div > div > div > div.MDTTD808z7GuhtYhNCiyx > form > div.IKVnscL2xZb4_HAppSRik > div > div > button._1JPTNwXTDV_vLByphy1l-O.M9c1UcWhIvzGEP-ZtlSLa._2bMXaBwkLZj8rpG7EdgcRt"),
+                          ("user", "http://media.test.itass.local/clients/users", "#root > div > div > div > header > button", "#fullName", "ц@ук-ецук ---@--", "#email", "teывап2@yanывапdex.r", "#fullName", "цук-ецук -----", "#email", "teывап2@yanывапdex.r", "#user-creating > div > div > div > button._1JPTNwXTDV_vLByphy1l-O.M9c1UcWhIvzGEP-ZtlSLa._2bMXaBwkLZj8rpG7EdgcRt"),
+                          ("prod", "http://media.test.itass.local/clients/customers/106/products", "#root > div > div > div > div.MDTTD808z7GuhtYhNCiyx > div._3FtI9K_rYuNqNUawu1Ow7K._2dqkqLWSBZAvRV2i3XPaE1 > header > button", "#name", "test3@yandex.ru№", "#mediaRegistrationCertificate", "test3@yandex.r№", "#circulation", "№city№", "#name", "test3@yandex.ru", "#root > div > div > div > div.MDTTD808z7GuhtYhNCiyx > div.IKVnscL2xZb4_HAppSRik > div > div > button._1JPTNwXTDV_vLByphy1l-O.M9c1UcWhIvzGEP-ZtlSLa._2bMXaBwkLZj8rpG7EdgcRt"),
+                          ("", "", "", "#site", "test4@yandex.ru", 79992244333, "test4@yandex.r", 12345, "city", 2020202024, 222444884, "")])
+def test_guest(browser, flag, links, buttons, selector, value, selector1, value1, selector2, value2, selector3, value3, button_save):
     # browser.get(link)
     # time.sleep(2)
     # browser.find_element_by_id("details-button").click()
@@ -39,40 +39,78 @@ def test_guest(browser, selector, value, selector1, value1, selector2, value2, s
     # browser.find_element_by_id("passwordInput").send_keys("Overlor1")
     # browser.find_element_by_id("submitButton").click()
 
-    page = MainPage(browser, link)  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
+    page = MainPage(browser, links)  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
     page.open()
 
     time.sleep(2)
-    input = browser.find_element_by_css_selector("#root > div > div > div > header > a > button")
+    input = browser.find_element_by_css_selector(buttons)
     input.click()
     time.sleep(1)
 
+    base_page = BasePage(browser, browser.current_url)
     mass_selector = [selector, selector1, selector2, selector3]
     mass_value = [value, value1, value2, value3]
 
     # input = browser.find_element(By.XPATH, "// *[ @ id = 'root'] / div / div[3] / form / article[1] / section / div / div[1] / label / div / div")
     # input.click()
 
+    if flag == "client":
+        input = browser.find_element(By.XPATH, "//*[@id='root']/div/div/div/div[3]/form/div[1]/span[2]")
+        status = input.text
+        assert status == "Анкета не отправлена"
 
-    input = browser.find_element(By.XPATH, "//*[@id='root']/div/div/div/div[3]/form/div[1]/span[2]")
-    status = input.text
-    assert status == "Анкета не отправлена"
+        # переключение на внутреннего клиента
+        input = browser.find_element_by_css_selector(
+            "#root > div > div > div > div.MDTTD808z7GuhtYhNCiyx > form > section:nth-child(3) > section > div > div._2z02StXLta1l1pMy9RZjqn._2KqIDt_bTCe7sb8_y5pdt0._3vXHzL1qtcips0rxKwEhuQ > label > div")
+        input.click()
 
-    # переключение на внутреннего клиента
-    input = browser.find_element_by_css_selector(
-        "#root > div > div > div > div.MDTTD808z7GuhtYhNCiyx > form > section:nth-child(3) > section > div > div._2z02StXLta1l1pMy9RZjqn._2KqIDt_bTCe7sb8_y5pdt0._3vXHzL1qtcips0rxKwEhuQ > label > div")
-    input.click()
+    if flag == "user":
+        input = browser.find_element(By.XPATH, "//*[@id='modal-root']/div/div/section/div/form/div/label")
+        status = input.text
+        assert status == "Имя Фамилия пользователя"
+
+        # ввод имени полользователя
+        input = browser.find_element_by_css_selector("#fullName")
+        input.send_keys("Имя")
+        # сохранить
+        input = browser.find_element_by_css_selector(
+            "#modal-root > div > div > section > div > form > button")
+        input.click()
+
+        input = browser.find_element_by_css_selector("#modal-root > div > div > section > div > form > div > span")
+        mess = input.text
+        if mess == "Некорректное значение":
+            input = browser.find_element_by_css_selector("#fullName")
+            input.send_keys(" Фамилия")
+            input = browser.find_element_by_css_selector(
+                "#modal-root > div > div > section > div > form > button")
+            input.click()
+
+    if flag == "prod":
+        input = browser.find_element(By.XPATH, "//*[@id='product-creating']/section[1]/section/section/div/div[1]/div[2]/div/label")
+        status = input.text
+        assert status == "Наименование продукта"
+
+        # клик по новый продукт
+        # input = browser.find_element_by_css_selector(
+        #     "#root > div > div > div > div.MDTTD808z7GuhtYhNCiyx > div._3FtI9K_rYuNqNUawu1Ow7K._2dqkqLWSBZAvRV2i3XPaE1 > header > button")
+        # input.click()
+        # time.sleep(1)
+        input = browser.find_element_by_css_selector(
+            "#product-creating > section:nth-child(1) > section > section > div > div:nth-child(2) > div._1BYAPcKJgAVhwLSvxdLQtV._2AYE17bNwF9fAiUDd3jicg._3FlCQKhOHMB0FDq6Lniosb._1CollOa9FvDSi8OuUmm1vf > div > div._1BYAPcKJgAVhwLSvxdLQtV._2AYE17bNwF9fAiUDd3jicg._3FlCQKhOHMB0FDq6Lniosb._1y9E9O3hdI5vdkkrArB_b > div > label")
+        input.click()
 
     i = 0
     for ii in mass_selector:
-
+        time.sleep(2)
         # поля
+        browser.find_element_by_css_selector(mass_selector[i]).clear()
         input = browser.find_element_by_css_selector(mass_selector[i])
         input.send_keys(mass_value[i])
 
         # клик по кнопке сохранить
-        browser.find_element_by_css_selector(
-            "#root > div > div > div > div.MDTTD808z7GuhtYhNCiyx > form > div.IKVnscL2xZb4_HAppSRik > div > div > button._1JPTNwXTDV_vLByphy1l-O.M9c1UcWhIvzGEP-ZtlSLa._2bMXaBwkLZj8rpG7EdgcRt").click()
+        input = browser.find_element_by_css_selector(button_save)
+        input.click()
         time.sleep(2)
         browser.execute_script('window.scrollTo(0,0);')
 
@@ -80,20 +118,25 @@ def test_guest(browser, selector, value, selector1, value1, selector2, value2, s
         input1 = browser.find_element_by_class_name("_2BdPeWfKXp-TXCYIUAZiad")
         status1 = input1.text
         assert status1 == "Некорректное значение"
-        time.sleep(3)
+        time.sleep(2)
 
         # преписка к значению в поле(изменение поля)
         input = browser.find_element_by_css_selector(mass_selector[i])
         input.send_keys("f")
 
-        base_page = BasePage(browser, browser.current_url)
-        assert base_page.is_element_present(By.CLASS_NAME, "_2BdPeWfKXp-TXCYIUAZiad")
+        # base_page = BasePage(browser, browser.current_url)
+        if flag == "client":
+            assert base_page.is_element_present(By.CLASS_NAME, "_2BdPeWfKXp-TXCYIUAZiad")
 
         base_page = BasePage(browser, browser.current_url)
         assert base_page.is_element_present(By.LINK_TEXT, "Некорректное значение") == False
 
-        input = browser.find_element_by_css_selector(mass_selector[i])
-        input.clear()
+        # проверка на наличие надписи "Некорректное значение"
+        # input1 = browser.find_element_by_class_name("_2BdPeWfKXp-TXCYIUAZiad")
+        # status1 = input1.text
+        # assert status1 == "Некорректное значение"
+
+        browser.find_element_by_css_selector(mass_selector[i])
         i = i + 1
 
 
