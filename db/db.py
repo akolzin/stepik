700efda976ecd0ce5a0499642e191190778ec21a
akolzin <59665736+akolzin@users.noreply.github.com>
2020-09-24 14:42:48 +0400

    +

--- db/db.py
+++ db/db.py
@@ -1,11 +1,64 @@
 import psycopg2
+import psycopg2.extras
 
-psycopg2.connect(
-    database="overlor",
-    user="overlor",
-    password="123",
-    host="127.0.0.1",
-    port="51715"
-)
+# не доделано из за conftest 1:04:33 в лекции 6 https://www.youtube.com/watch?v=4SiT_LCOOU4&feature=youtu.be
+# декоратор
+def toDict(func):
+    def wrapper(*args, **kwargs):
+        rows = func(*args, **kwargs)
+        arr = []
+        for row in rows:
+            d = {}
+            for key in row:
+                d[key] = row[key]
+            arr.append(d)
+        return arr
 
-print("ffffff")
+    return wrapper
+
+
+class DB:
+    def __init__(self):
+        try:
+            self.connect = psycopg2.connect(
+                database="overlor",
+                user="overlor",
+                password="1234",
+                host="127.0.0.1",
+                port="5432"
+            )
+
+            self.cursor = self.connect.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
+            print("'  id':  , 'name': '   ', ' login': '   '")
+        except ValueError as err:
+            print("ggggg")
+
+    def __del__(self):
+        self.cursor.close()
+        self.connect.close()
+
+    @toDict
+    def getAllUsers(self):
+        self.cursor.execute("SELECT id, name, login FROM users")
+        return self.cursor.fetchall()
+
+    @toDict
+    def getTestResults(self):
+        query = "SELECT id, name, result, date_time FROM tests ORDER BY date_time"
+        self.cursor.execute(query)
+        return self.cursor.fetchall()
+
+    def insertTestResulr(self, name, result):
+        query = "INSERT INTO tests (name, result, date_time) VALUES (%s, %s, now())"
+        self.cursor.execute(query, (name, result))
+        self.connect.commit()
+        return True
+
+db = DB()
+
+#users = db.getAllUsers()
+#users = db.getTestResults()
+users = db.insertTestResulr('3', True)
+
+
+print(users)
